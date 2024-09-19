42ca003b0201f686dcac6bdcd4735e8847554c4d
Omar Russo Castillo <omar.russo23@gmail.com>
2024-09-19 11:32:58 +0200

    alpha

--- obj/Debug/net6.0/CodeApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/CodeApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c28c9fbc32e623d8c87b18fe9a200723dd66be4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
